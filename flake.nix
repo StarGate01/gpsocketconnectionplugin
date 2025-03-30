6b2475212f302bd7662aa9c9b6f8f84e830b5535
Christoph Honal <christoph.honal@web.de>
2025-03-30 18:54:26 +0200

    Add nix config

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs = {
+    nixpkgs.url = "github:StarGate01/nixpkgs/gpshell-python";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      devShell.x86_64-linux =
+        pkgs.mkShell {
+          shellHook = ''
+          '';
+
+          buildInputs = with pkgs; [
+            gdb
+            gcc
+            pkg-config
+            cmake
+            gnumake
+            globalplatform
+            pcsclite
+            (python3.withPackages (ps: with ps; [
+              (pkgs.python3Packages.pyglobalplatform.overrideAttrs (oldAttrs: {
+                src = pkgs.fetchFromGitHub {
+                  owner = "StarGate01";
+                  repo = "pyglobalplatform";
+                  rev = "master";
+                  sha256 = "sha256-X2ZOr6/P8HLbi9recYj6CtfXOWHjbciHkBmAKk3+YWM=";
+                };
+              }))
+            ]))
+          ];
+        };
+    };
+}
